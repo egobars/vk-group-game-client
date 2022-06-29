f695f6d68daca1c4bc682d2157e1fcc3fdeec558
Egor Molchan <egobars@yandex.ru>
2022-06-30 00:43:11 +0300

    some changes

--- src/main_body.js
+++ src/main_body.js
@@ -7,15 +7,33 @@ class MainBody extends React.Component {
         this.state = {
             button_class: '',
             block_class: 'right_',
+            first_image_url: '',
+            second_image_url: '',
+            first_name: '',
+            second_name: '',
+            first_sub_count: 0,
+            second_sub_count: 0
         };
     }
 
     moveCenterBlock() {
         this.setState({block_class: ''});
+        fetch('/get/group')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({first_image_url: data.photo, first_name: data.name, first_sub_count: data.members_count});
+            });
+        fetch('/get/group')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({second_image_url: data.photo, second_name: data.name, second_sub_count: data.members_count});
+            });
     }
 
     moveRightBlock() {
-        this.setState({block_class: 'right_'});
+        this.setState({block_class: 'right_', first_name: '', second_name: ''});
         setTimeout(() => this.moveCenterBlock(), 10);
     }
 
@@ -32,6 +50,46 @@ class MainBody extends React.Component {
         }, 500);
     }
 
+    genFirstVariant() {
+        let to_return = [];
+        if (this.state.first_name == '') {
+            to_return.push(
+                <span>...</span>
+            )
+        } else {
+            to_return.push(
+                <div className="stripe"/>
+            )
+            to_return.push(
+                <img className="variant_image" src={this.state.first_image_url} alt="first_image"/>
+            )
+            to_return.push(
+                <span>{this.state.first_name}</span>
+            )
+        }
+        return to_return;
+    }
+
+    genSecondVariant() {
+        let to_return = [];
+        if (this.state.second_name == '') {
+            to_return.push(
+                <span>...</span>
+            )
+        } else {
+            to_return.push(
+                <div className="stripe"/>
+            )
+            to_return.push(
+                <img className="variant_image" src={this.state.second_image_url} alt="second_image"/>
+            )
+            to_return.push(
+                <span>{this.state.second_name}</span>
+            )
+        }
+        return to_return;
+    }
+
     render() {
         let to_return;
         if (this.state.button_class != 'hidden_') {
@@ -46,8 +104,12 @@ class MainBody extends React.Component {
             to_return = (
                 <div className="wrapper">
                     <div className={this.state.block_class + "block"}>
-                        <div className={"variant"} onClick={() => this.moveLeftBlock()}/>
-                        <div className={"variant"} onClick={() => this.moveLeftBlock()}/>
+                        <div className={"variant"} onClick={() => this.moveLeftBlock()}>
+                            {this.genFirstVariant()}
+                        </div>
+                        <div className={"variant"} onClick={() => this.moveLeftBlock()}>
+                            {this.genSecondVariant()}
+                        </div>
                     </div>
                 </div>
             )
